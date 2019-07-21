103672be76d62db96a561954d9c05a2912bbb5da
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-21 13:10:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,9 +35,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash = {
-    :name => "Steve"
-  }
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
